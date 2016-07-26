4aae3628ec46f9ec4856d77ca943e045b08e63e0
Karan <kavishw@microsoft.com>
2016-07-26 16:03:20 -0700

    beginning twilio integration

--- textPPTServer/app.js
+++ textPPTServer/app.js
@@ -1,18 +1,28 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var twilio = require('twilio');
+
 const port = 3000
 
 app.get('/', (request, response) => {  
   console.log("gotten")
-  response.send('Hello from Express!')
+
+  console.log(`server is listening on ${port}`)
+
+  var resp = new twilio.TwimlResponse();
+  resp.say('Testing Twilio and node.js');
+
+   response.writeHead(200, {
+        'Content-Type':'text/xml'
+    });
+    response.end(resp.toString());
 })
 
 app.listen(port, (err) => {  
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 })
 
