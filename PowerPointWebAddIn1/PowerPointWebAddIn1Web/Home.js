5105bfbfb7b4a738297acf4ab9f77b2658d99a35
prafmathur <prafulm@umich.edu>
2016-07-26 14:58:33 -0700

    Added ppt app files

---
+++ PowerPointWebAddIn1/PowerPointWebAddIn1Web/Home.js
@@ -0,0 +1,103 @@
+﻿/// <reference path="/Scripts/FabricUI/MessageBanner.js" />
+
+
+(function () {
+    "use strict";
+
+    var Globals = { activeViewHandler: 0, firstSlideId: 0 };
+    var messageBanner;
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Initialize the FabricUI notification mechanism and hide it
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+
+            getSelectedRange();
+            $('#get-data-from-selection').click(getDataFromSelection);
+            $('#goToFirstSlide').click(goToFirstSlide);
+
+        });
+    };
+
+    // Reads data from current document selection and displays a notification
+    function getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    showNotification('The selected text is:', '"' + result.value + '"');
+                } else {
+                    showNotification('Error:', result.error.message);
+                }
+            }
+        );
+    }
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notificationHeader").text(header);
+        $("#notificationBody").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+
+    function getSelectedRange() {
+        // Get the id, title, and index of the current slide (or selected slides) and store the first slide id */
+        Globals.firstSlideId = 0;
+
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange, function (asyncResult) {
+            if (asyncResult.status == "failed") {
+                showNotification("Action failed with error: " + asyncResult.error.message);
+            }
+            else {
+                Globals.firstSlideId = asyncResult.value.slides[0].id;
+                showNotification(JSON.stringify(asyncResult.value));
+            }
+        });
+    }
+
+
+    function goToFirstSlide() {
+
+        console.log('go')
+
+        var request = new XMLHttpRequest();
+
+
+
+        var socket = io.connect('http://localhost:3000');
+        socket.on('news', function (data) {
+            console.log(data);
+            socket.emit('my other event', { my: 'data' });
+        });
+
+        Office.context.document.goToByIdAsync(Globals.firstSlideId, Office.GoToType.Slide, function (asyncResult) {
+            if (asyncResult.status == "failed") {
+                showNotification("Action failed with error: " + asyncResult.error.message);
+            }
+            else {
+                showNotification("Navigation successful");
+            }
+        });
+    }
+
+    function goToSlideByIndex() {
+        var goToFirst = Office.Index.First;
+        var goToLast = Office.Index.Last;
+        var goToPrevious = Office.Index.Previous;
+        var goToNext = Office.Index.Next;
+
+        Office.context.document.goToByIdAsync(goToNext, Office.GoToType.Index, function (asyncResult) {
+            if (asyncResult.status == "failed") {
+                showNotification("Action failed with error: " + asyncResult.error.message);
+            }
+            else {
+                showNotification("Navigation successful");
+            }
+        });
+    }
+
+
+})();
