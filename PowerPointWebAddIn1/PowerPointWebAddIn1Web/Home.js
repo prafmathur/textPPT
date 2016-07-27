abc06fbea3773e1b884812d9fdf8a3c42d42abaf
prafmathur <prafulm@umich.edu>
2016-07-27 15:17:53 -0700

    removed server to its own repo

--- PowerPointWebAddIn1/PowerPointWebAddIn1Web/Home.js
+++ PowerPointWebAddIn1/PowerPointWebAddIn1Web/Home.js
@@ -15,10 +15,10 @@
             messageBanner = new fabric.MessageBanner(element);
             messageBanner.hideBanner();
 
-            getSelectedRange();
             setInterval(makeRequest, 5000)
         });
     };
+
 
     // Reads data from current document selection and displays a notification
     function getDataFromSelection() {
@@ -41,56 +41,53 @@
         messageBanner.toggleExpansion();
     }
 
-    function getSelectedRange() {
-        // Get the id, title, and index of the current slide (or selected slides) and store the first slide id */
-        Globals.firstSlideId = 0;
-
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.SlideRange, function (asyncResult) {
-            if (asyncResult.status == "failed") {
-                showNotification("Action failed with error: " + asyncResult.error.message);
-            }
-            else {
-                Globals.firstSlideId = asyncResult.value.slides[0].id;
-                showNotification(JSON.stringify(asyncResult.value));
-            }
-        });
-    }
-
 
     function makeRequest() {
         $.ajax({
+            //url: "http://52.161.27.50:3000",
             url: "http://localhost:3000",
             crossDomain: true,
             dataType: "jsonp",
             success: function (data) {
-                console.log(data)
+                console.log("Recieved command")
+                interpretCommand("next");
             },
             error: function (e) {
+                console.log("Error:")
                 console.log(e)
             }
         });
     }
 
+    function interpretCommand(command) {
+        command = command.replace(/\s+/g, '').toLowerCase();
+        if (command == "next") {
+            console.log("Going to next slide");
+            goToSlide(Office.Index.Next)
+        }
+        if (command == "previous") {
+            console.log("Going to previous slide");
+            goToSlide(Office.Index.Previous)
+        }
+        //Office.Index.First;
+        //Office.Index.Last;
+    }
 
 
     function goToFirstSlide() {
         Office.context.document.goToByIdAsync(Globals.firstSlideId, Office.GoToType.Slide, function (asyncResult) {
             if (asyncResult.status == "failed") {
-                showNotification("Action failed with error: " + asyncResult.error.message);
+                console.log("Action failed with error: " + asyncResult.error.message);
             }
             else {
-                showNotification("Navigation successful");
+                console.log("Navigation successful");
             }
         });
     }
 
-    function goToSlideByIndex() {
-        var goToFirst = Office.Index.First;
-        var goToLast = Office.Index.Last;
-        var goToPrevious = Office.Index.Previous;
-        var goToNext = Office.Index.Next;
 
-        Office.context.document.goToByIdAsync(goToNext, Office.GoToType.Index, function (asyncResult) {
+    function goToSlide(whichSlide) {
+        Office.context.document.goToByIdAsync(whichSlide, Office.GoToType.Index, function (asyncResult) {
             if (asyncResult.status == "failed") {
                 showNotification("Action failed with error: " + asyncResult.error.message);
             }
@@ -99,6 +96,45 @@
             }
         });
     }
+
+
+    //function goToNextSlide() {
+
+    //    Office.context.document.goToByIdAsync(4, Office.GoToType.Index, function (asyncResult) {
+    //        if (asyncResult.status == "failed") {
+    //            showNotification("Action failed with error: " + asyncResult.error.message);
+    //        }
+    //        else {
+    //            showNotification("Navigation successful");
+    //        }
+    //    });
+    //    //Office.context.document.goToByIdAsync(Office.Index.Next, Office.GoToType.Index, function (asyncResult) {
+    //    //    if (asyncResult.status == "failed") {
+    //    //        showNotification("Action failed with error: " + asyncResult.error.message);
+    //    //    }
+    //    //    else {
+    //    //        showNotification("Navigation successful");
+    //    //    }
+    //    //});
+
+
+    //}
+
+    //function goToSlideByIndex() {
+    //    var goToFirst = Office.Index.First;
+    //    var goToLast = Office.Index.Last;
+    //    var goToPrevious = Office.Index.Previous;
+    //    var goToNext = Office.Index.Next;
+
+    //    Office.context.document.goToByIdAsync(goToNext, Office.GoToType.Index, function (asyncResult) {
+    //        if (asyncResult.status == "failed") {
+    //            showNotification("Action failed with error: " + asyncResult.error.message);
+    //        }
+    //        else {
+    //            showNotification("Navigation successful");
+    //        }
+    //    });
+    //}
 
 
 })();
