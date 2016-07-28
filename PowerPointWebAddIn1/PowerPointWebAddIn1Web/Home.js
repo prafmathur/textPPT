1bb4034def72cbb9250837d757d54fd5a8cd32d7
prafmathur <prafulm@umich.edu>
2016-07-28 10:51:16 -0700

    final UX changes

--- PowerPointWebAddIn1/PowerPointWebAddIn1Web/Home.js
+++ PowerPointWebAddIn1/PowerPointWebAddIn1Web/Home.js
@@ -5,58 +5,40 @@
     "use strict";
 
     var Globals = { activeViewHandler: 0, firstSlideId: 0 };
-    var messageBanner;
+    var firstTime = true;
+    var recentTimestamp;
 
     // The initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
+        makeRequest()
         $(document).ready(function () {
-            // Initialize the FabricUI notification mechanism and hide it
-            var element = document.querySelector('.ms-MessageBanner');
-            messageBanner = new fabric.MessageBanner(element);
-            messageBanner.hideBanner();
-
-            setInterval(makeRequest, 5000)
+            setInterval(makeRequest, 500)
         });
     };
 
-
-    // Reads data from current document selection and displays a notification
-    function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    showNotification('Error:', result.error.message);
-                }
-            }
-        );
-    }
-
-    // Helper function for displaying notifications
-    function showNotification(header, content) {
-        $("#notificationHeader").text(header);
-        $("#notificationBody").text(content);
-        messageBanner.showBanner();
-        messageBanner.toggleExpansion();
-    }
-
-
     function makeRequest() {
         $.ajax({
-            //url: "http://52.161.27.50:3000",
-            url: "http://localhost:3000",
+            url: "https://textppt.herokuapp.com",
             crossDomain: true,
             dataType: "jsonp",
             success: function (data) {
-                console.log("Recieved command")
-                interpretCommand("next");
+                if (firstTime) {
+                    firstTime = false
+                }
+                else if (recentTimestamp != data.timestamp) {
+                    interpretCommand(data.command);
+                }
+                recentTimestamp = data.timestamp;
             },
             error: function (e) {
                 console.log("Error:")
                 console.log(e)
             }
         });
+    }
+
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
     }
 
     function interpretCommand(command) {
@@ -69,13 +51,23 @@
             console.log("Going to previous slide");
             goToSlide(Office.Index.Previous)
         }
+        else {
+            console.log(command);
+            for (var i = 0; i < command.length; i++) {
+                if (isFinite(command[i])) {
+                    goToSlide(command[i])
+                    break
+                }
+            }
+        }
+
         //Office.Index.First;
         //Office.Index.Last;
     }
 
 
-    function goToFirstSlide() {
-        Office.context.document.goToByIdAsync(Globals.firstSlideId, Office.GoToType.Slide, function (asyncResult) {
+    function goToSlide(whichSlide) {
+        Office.context.document.goToByIdAsync(whichSlide, Office.GoToType.Index, function (asyncResult) {
             if (asyncResult.status == "failed") {
                 console.log("Action failed with error: " + asyncResult.error.message);
             }
@@ -84,57 +76,5 @@
             }
         });
     }
-
-
-    function goToSlide(whichSlide) {
-        Office.context.document.goToByIdAsync(whichSlide, Office.GoToType.Index, function (asyncResult) {
-            if (asyncResult.status == "failed") {
-                showNotification("Action failed with error: " + asyncResult.error.message);
-            }
-            else {
-                showNotification("Navigation successful");
-            }
-        });
-    }
-
-
-    //function goToNextSlide() {
-
-    //    Office.context.document.goToByIdAsync(4, Office.GoToType.Index, function (asyncResult) {
-    //        if (asyncResult.status == "failed") {
-    //            showNotification("Action failed with error: " + asyncResult.error.message);
-    //        }
-    //        else {
-    //            showNotification("Navigation successful");
-    //        }
-    //    });
-    //    //Office.context.document.goToByIdAsync(Office.Index.Next, Office.GoToType.Index, function (asyncResult) {
-    //    //    if (asyncResult.status == "failed") {
-    //    //        showNotification("Action failed with error: " + asyncResult.error.message);
-    //    //    }
-    //    //    else {
-    //    //        showNotification("Navigation successful");
-    //    //    }
-    //    //});
-
-
-    //}
-
-    //function goToSlideByIndex() {
-    //    var goToFirst = Office.Index.First;
-    //    var goToLast = Office.Index.Last;
-    //    var goToPrevious = Office.Index.Previous;
-    //    var goToNext = Office.Index.Next;
-
-    //    Office.context.document.goToByIdAsync(goToNext, Office.GoToType.Index, function (asyncResult) {
-    //        if (asyncResult.status == "failed") {
-    //            showNotification("Action failed with error: " + asyncResult.error.message);
-    //        }
-    //        else {
-    //            showNotification("Navigation successful");
-    //        }
-    //    });
-    //}
-
 
 })();
---
+++ PowerPointWebAddIn1/PowerPointWebAddIn1Web/obj/Release/Package/PackageTmp/Home.js
@@ -0,0 +1,66 @@
+﻿/// <reference path="/Scripts/FabricUI/MessageBanner.js" />
+
+
+(function () {
+    "use strict";
+
+    var Globals = { activeViewHandler: 0, firstSlideId: 0 };
+    var firstTime = true;
+    var recentTimestamp;
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        makeRequest()
+        $(document).ready(function () {
+            setInterval(makeRequest, 2000)
+        });
+    };
+
+    function makeRequest() {
+        $.ajax({
+            url: "https://textppt.herokuapp.com",
+            crossDomain: true,
+            dataType: "jsonp",
+            success: function (data) {
+                if (firstTime) {
+                    firstTime = false
+                }
+                else if (recentTimestamp != data.timestamp) {
+                    interpretCommand(data.command);
+                }
+                recentTimestamp = data.timestamp;
+            },
+            error: function (e) {
+                console.log("Error:")
+                console.log(e)
+            }
+        });
+    }
+
+    function interpretCommand(command) {
+        command = command.replace(/\s+/g, '').toLowerCase();
+        if (command == "next") {
+            console.log("Going to next slide");
+            goToSlide(Office.Index.Next)
+        }
+        if (command == "previous") {
+            console.log("Going to previous slide");
+            goToSlide(Office.Index.Previous)
+        }
+        //Office.Index.First;
+        //Office.Index.Last;
+    }
+
+
+    function goToSlide(whichSlide) {
+        Office.context.document.goToByIdAsync(whichSlide, Office.GoToType.Index, function (asyncResult) {
+            if (asyncResult.status == "failed") {
+                console.log("Action failed with error: " + asyncResult.error.message);
+            }
+            else {
+                console.log("Navigation successful");
+            }
+        });
+    }
+
+})();
